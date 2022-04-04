ce973345a7c230cf0612cc4711641922ac207bc4
Ярослав <budancevya@mail.ru>
2022-04-04 18:41:48 +0300

    Fix bags with neural

--- face_detector.py
+++ face_detector.py
@@ -1,6 +1,6 @@
 import cv2
 import math_cam
-import arduino
+#import arduino
 import datetime
 
 
@@ -8,7 +8,7 @@ class Face_detection:
     now_time = datetime.datetime.now().second
     face_cascade_db = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     face_cascade_ff = cv2.CascadeClassifier("haarcascade_profileface.xml")
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     delta_x = 0
     delta_y = 0
 
@@ -28,8 +28,8 @@ class Face_detection:
                     math_cam.delta_angle = self.delta_x
                     if (datetime.datetime.now().second - self.now_time) % 1.5 == 0 and datetime.datetime.now().second != count_res:
                         count_res = datetime.datetime.now().second
-                        arduino.get_angle(math_cam.delta())
-                        arduino.get_santimetr()
+                        #arduino.get_angle(math_cam.delta())
+                        #arduino.get_santimetr()
 
             elif len(face_f) != 0:
                 for (x, y, w, h) in face_f:
@@ -39,21 +39,20 @@ class Face_detection:
                     math_cam.delta_angle = self.delta_x
                     if (datetime.datetime.now().second - self.now_time) % 1.5 == 0 and datetime.datetime.now().second != count_res:
                         count_res = datetime.datetime.now().second
-                        arduino.get_angle(math_cam.delta())
-                        arduino.get_santimetr()
+                        #arduino.get_angle(math_cam.delta())
+                        #arduino.get_santimetr()
             else:
                 f_f = cv2.flip(img_gray, 1)
                 face_ff = self.face_cascade_ff.detectMultiScale(f_f, 1.1, 19)
-                face_ff = cv2.flip(face_ff, 1)
                 for (x, y, w, h) in face_ff:
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    cv2.rectangle(img, (x+w, y), (x, y + h), (0, 255, 0), 2)
                     self.delta_x = x + w // 2 - 320
                     self.delta_y = -(y + h // 2 - 240)
                     math_cam.delta_angle = self.delta_x
                     if (datetime.datetime.now().second - self.now_time) % 1.5 == 0 and datetime.datetime.now().second != count_res:
                         count_res = datetime.datetime.now().second
-                        arduino.get_angle(math_cam.delta())
-                        arduino.get_santimetr()
+                        #arduino.get_angle(math_cam.delta())
+                        #arduino.get_santimetr()
 
             cv2.imshow('rez', img)
             if cv2.waitKey(1) & 0xff == ord('q'):
